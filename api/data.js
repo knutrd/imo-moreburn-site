bce218fcb27f82d52fbd9ecf2cbdd795f6b0ee1e
knutrd <68446876+knutrd@users.noreply.github.com>
2026-06-02 21:02:53 +0200

    Add files via upload

--- api/data.js
+++ api/data.js
@@ -119,6 +119,7 @@ window.SITE_DATA = {
   burns: [
     {
       date: "01/06/2026",
+      forMonth: "2026-05",
       amount: "27,500 IMO",
       usdAmount: 10500,
       txTotalAmount: "41,869 IMO",
@@ -129,6 +130,7 @@ window.SITE_DATA = {
     },
     {
       date: "30/04/2026",
+      forMonth: "2026-04",
       amount: "2,083 IMO",
       usdAmount: 1000,
       volumeAtBurn: 324449,                 // total cumulative volume at burn time (resets the "Awaiting burn" counter)
